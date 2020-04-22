7a8ad3c73eb0d0f024a5cb1ee2321043b8db8c23
Claire <claire.cy.t@gmail.com>
2020-04-22 01:26:54 -0400

    Add counter screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import { Text, StyleSheet,View, Button, TouchableOpacity } from 'react-native';
+import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
 const HomeScreen = ({navigation}) => {
 
-  return (
-      <View>
-        <Text style={styles.text}>Hi There!</Text>
-        <Button
-            onPress = {() => navigation.navigate('Components')}
-            title = "Go to Components Demo"/>
-          <Button
-              onPress = {() => navigation.navigate('List')}
-              title = "Go to List Demo" />
-              <Button
-              onPress = {() => navigation.navigate('Image')}
-              title = "Go to Image Demo" />
-      </View>
+    return (
+        <View>
+            <Text style={styles.text}>Hi There!</Text>
+            <Button
+                onPress={() => navigation.navigate('Components')}
+                title="Go to Components Demo"/>
+            <Button
+                onPress={() => navigation.navigate('List')}
+                title="Go to List Demo"/>
+            <Button
+                onPress={() => navigation.navigate('Image')}
+                title="Go to Image Demo"/>
+            <Button
+                onPress={() => navigation.navigate('Counter')}
+                title="Go to Count Demo"/>
+        </View>
 
-  );
+    );
 
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+    text: {
+        fontSize: 30
+    }
 });
 
 export default HomeScreen;
